b0c2e22496ad72dd7bb1928d2ac3f58c1a98ddda
Nicholas <koalrube@gmail.com>
2024-03-09 17:03:17 -0500

    got the gui working better

--- Core.lua
+++ Core.lua
@@ -1,10 +1,17 @@
-Banshee = LibStub("AceAddon-3.0"):NewAddon("Banshee", "AceConsole-3.0");
+Banshee = LibStub("AceAddon-3.0"):NewAddon("Banshee", "AceConsole-3.0", "AceHook-3.0");
 
 function Banshee:OnInitialize()
     self:Print("Banshee is active");
-    self:RegisterChatCommand("banshee", "ChatCommand")
+    self:RegisterChatCommand("banshee", "ShowGUI");
+
+    self:RegisterChatCommand("myguild", "GetNumGuildMembers")
 end
 
-function Banshee:ChatCommand()
+function Banshee:GetNumGuildMembers()
+    local numTotal, numOnline, numOnlineAndMobile = GetNumGuildMembers();
+    self:Print("Number of online members: " .. numOnline);
+end
+
+function Banshee:ShowGUI()
     self:ShowFrame()
 end
--- GUI.lua
+++ GUI.lua
@@ -1,18 +1,24 @@
 local frameShown = false;
 
+
+local function createFrame()
+    local AceGUI = LibStub("AceGUI-3.0");
+    local frame = AceGUI:Create("Frame");
+    frame:SetTitle("Banshee");
+    frame:SetStatusText("AceGUI-3.0 Example Container Frame");
+    frame:SetCallback("OnClose", function(widget)
+        AceGUI:Release(widget)
+        frameShown = false
+    end
+    )
+end
+
+
 function Banshee:ShowFrame()
     if frameShown then
         return
     end
 
     frameShown = true
-    local AceGUI = LibStub("AceGUI-3.0");
-    local frame = AceGUI:Create("Frame");
-    frame:SetTitle("Banshee");
-    --frame:SetStatusText("AceGUI-3.0 Example Container Frame");
-    frame:SetCallback("OnClose", function(widget)
-        AceGUI:Release(widget)
-        frameShown = false
-    end
-    )
+    createFrame();
 end
